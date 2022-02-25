4e1c9f2be1d1f3ead29fa5f92988769dbb2171e0
Paul <paul.fermey@epitech.eu>
2022-02-25 15:13:48 +0100

    [*] web_client/ : change favicon and update App.js

--- web_client/src/App.js
+++ web_client/src/App.js
@@ -22,7 +22,7 @@ function App() {
             />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <PokemonCard 
+            <PokemonCard
               name={'bulbi'}
             />
           </Grid>
